b65de491cb0f90ae41df2a8e1c12d758b28a9c06
kahunas <132077670+kahunas@users.noreply.github.com>
2024-12-18 21:44:29 +0200

    Update Program.cs

--- ApiaryAdmin/Program.cs
+++ ApiaryAdmin/Program.cs
@@ -67,7 +67,6 @@ public class Program
         var dbContext = scope.ServiceProvider.GetRequiredService<ApiaryDbContext>();
         dbContext.Database.Migrate();
         
-
         var dbSeeder = scope.ServiceProvider.GetRequiredService<AuthSeeder>();
         dbSeeder.SeedAsync();
 
